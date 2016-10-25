dd7e417f54ee6af961a3efd0c94a1646a20feb61
Tucq <tucq88@gmail.com>
2016-10-25 03:25:33 +0000

    Tweak settings for Vagrantbox + debug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+
+  # For Vagrant box
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
+  config.web_console.whiny_requests = false
 end
